af7272000427e06ee2e1e2d5f5502cbefc4b5f82
adretokhan <adreto.khan@gmail.com>
2022-01-07 11:55:09 -0800

    testing

--- src/welcome.java
+++ src/welcome.java
@@ -31,8 +31,8 @@ public class welcome extends javax.swing.JFrame {
         mainlogo = new javax.swing.JLabel();
         maintitle = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
-        Buytickets = new javax.swing.JButton();
         rateus = new javax.swing.JButton();
+        buytickets = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -84,17 +84,17 @@ public class welcome extends javax.swing.JFrame {
             .addGap(0, 113, Short.MAX_VALUE)
         );
 
-        Buytickets.setBackground(new java.awt.Color(51, 51, 255));
-        Buytickets.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        Buytickets.setForeground(new java.awt.Color(255, 255, 255));
-        Buytickets.setText("Rate Us");
-        Buytickets.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-
         rateus.setBackground(new java.awt.Color(51, 51, 255));
         rateus.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         rateus.setForeground(new java.awt.Color(255, 255, 255));
-        rateus.setText("Buy Tickets");
+        rateus.setText("Rate Us");
         rateus.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        buytickets.setBackground(new java.awt.Color(51, 51, 255));
+        buytickets.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        buytickets.setForeground(new java.awt.Color(255, 255, 255));
+        buytickets.setText("Buy Tickets");
+        buytickets.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -105,8 +105,8 @@ public class welcome extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Buytickets, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(rateus, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(rateus, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buytickets, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(323, 323, 323))
         );
         jPanel1Layout.setVerticalGroup(
@@ -114,9 +114,9 @@ public class welcome extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(Heading, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
-                .addComponent(rateus, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(buytickets, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(Buytickets, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(rateus, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(56, 56, 56)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -171,8 +171,8 @@ public class welcome extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Buytickets;
     private javax.swing.JPanel Heading;
+    private javax.swing.JButton buytickets;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JLabel mainlogo;
